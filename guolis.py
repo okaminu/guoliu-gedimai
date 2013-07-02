0ee1ad41a1f7b2b2447f8dcf3a0e5f1bebd74b0d
Aurimas Degutis <aurimasdgt@gmail.com>
2013-07-02 23:23:15 +0300

    fixed major bugs: the values were saved in class variables, so the values were global during different objects, now values live as long as object lives, no global scope values. object is destroyed at the end, all values are referenced through object instead of class reference. Class variables are replaced with object variables.

--- guolis.py
+++ guolis.py
@@ -8,107 +8,110 @@ import wx
 #import threading
 
 class Signal:
-    _fileCol = 3
-    _lastFigure = 0
-    _rolls = 0
-    _rmsOrig = ''
-    _rmsClean = ''
-    _rmsMean = ''
-    _rmsCleanSignal = ''
-    _frameSize = 0
-    _rolls= 0
-    _limit = 0
-    _skip = 0
-    _limit = 0
-    _freMark = 0.0
-    _displayParams = 'g'
-    _originalData = []
-    _originalDataFreq = []
-    _cleanData = []
-    _cleanDataFreq = []
-    _meanFrame = []
-    _meanFrameFreq = []
-    _cleanTimeFrame = []
-    _cleanFreqFrame = [] #this one is made when each clean frame is converted to Freq and stacked (Freq frames stacked)
-    _cleanTimeFrameFreq = [] # this one is made when full cleaned signal time interval is converted to freq spectrum
-    _SingleRollTime = 0;
+
+    def initValues(self):
+        self._fileCol = 3
+        self._lastFigure = 0
+        self._rolls = 0
+        self._rmsOrig = ''
+        self._rmsClean = ''
+        self._rmsMean = ''
+        self._rmsCleanSignal = ''
+        self._frameSize = 0
+        self._rolls= 0
+        self._limit = 0
+        self._skip = 0
+        self._limit = 0
+        self._freMark = 0.0
+        self._displayParams = 'g'
+        self._originalData = []
+        self._originalDataFreq = []
+        self._cleanData = []
+        self._cleanDataFreq = []
+        self._meanFrame = []
+        self._meanFrameFreq = []
+        self._cleanTimeFrame = []
+        self._cleanFreqFrame = [] #this one is made when each clean frame is converted to Freq and stacked (Freq frames stacked)
+        self._cleanTimeFrameFreq = [] # this one is made when full cleaned signal time interval is converted to freq spectrum
+        self._SingleRollTime = 0;
 
     def __init__(self, time, frameSize, skip, skipFramesTime, freMark, singleRollTime):
-        Signal._SingleRollTime = int(singleRollTime)
-        Signal._rolls = self.convertTimeToRolls(time)
-        Signal._frameSize = int(frameSize)
-        Signal._skip = int(skip)
+        self.initValues();
+        self._SingleRollTime = int(singleRollTime)
+        self._rolls = self.convertTimeToRolls(time)
+        self._frameSize = int(frameSize)
+        self._skip = int(skip)
         skipFrames = self.convertTimeToRolls(skipFramesTime)
-        Signal._skip = Signal._skip + (skipFrames * Signal._frameSize)
-        Signal._limit = Signal._rolls * Signal._frameSize
-        Signal._limit = Signal._skip + Signal._limit
-        Signal._freMark = float(freMark)
-        Signal._displayParams = Signal._displayParams
+        self._skip = self._skip + (skipFrames * self._frameSize)
+        self._limit = self._rolls * self._frameSize
+        self._limit = self._skip + self._limit
+        self._freMark = float(freMark)
+        self._displayParams = self._displayParams
 
     def convertTimeToRolls(self, time):
         msTime = int(float(time)) * 1000
-        return int(float(msTime / Signal._SingleRollTime))
+        return int(float(msTime / self._SingleRollTime))
 
     def convertPointsToRolls(self, points):
-        return float(points / Signal._frameSize)
+        return float(points / self._frameSize)
 
     def convertRollsToTime(self, rolls):
-        rollsMs = float(rolls * Signal._SingleRollTime)
+        rollsMs = float(rolls * self._SingleRollTime)
         return  float(rollsMs / 1000)
 
 
     def _loadOriginal_File(self, location):
         fileData1 = open(location, "r")
-        dialog = wx.ProgressDialog('Skaiciuoja duomenis', 'Prasome palaukti', Signal._limit, style=wx.PD_REMAINING_TIME)
+        dialog = wx.ProgressDialog('Skaiciuoja duomenis', 'Prasome palaukti', self._limit, style=wx.PD_REMAINING_TIME)
 
-        for itera in range(Signal._limit):
+        for itera in range(self._limit):
             line = fileData1.readline()
-            if itera < Signal._skip:
+            if itera < self._skip:
                 continue
             dataRaw = re.split("\t", line)
             data = float(dataRaw[self._fileCol-1])
-            Signal._originalData.append(data)
+            self._originalData.append(data)
             # dialog.Update(itera)
         wx.CallAfter(dialog.Destroy)
 
     def _calcMeanFrame(self):
 
         bufIter = 0
-        buffer = [0] * Signal._frameSize
-        originalData = Signal._originalData
-        frameSize = Signal._frameSize
+        buffer = [0] * self._frameSize
+        originalData = self._originalData
+        frameSize = self._frameSize
 
         for iter in range(len(originalData)-frameSize):
             buffer[bufIter] = (buffer[bufIter] + originalData[iter]) / 2
             bufIter = bufIter+1
             if bufIter == frameSize:
                 bufIter=0
-        Signal._meanFrame = buffer
+        self._meanFrame = buffer
 
 
 
     def _cleanSignal(self):
 
         bufIter = 0
-        originalData = Signal._originalData
-        frameSize = Signal._frameSize
+        originalData = self._originalData
+        frameSize = self._frameSize
         cleanData=[0]* (len(originalData)-frameSize)
-        meanFrame = Signal._meanFrame
+        meanFrame = self._meanFrame
 
         for itera in range(len(originalData)-frameSize):
             cleanData[itera] = (originalData[itera] - meanFrame[bufIter]) / 2
             bufIter = bufIter+1
             if bufIter == frameSize:
                 bufIter=0
-        Signal._cleanData = cleanData
+        self._cleanData = cleanData
 
     def _stackCleanSignalFrames_Time_Freq(self):
         bufIter = 0
-        frameSize = Signal._frameSize
+        frameSize = self._frameSize
         cleanTimeFrame = [0] * frameSize
         cleanFreqFrame = [0]*((frameSize/2)+1)
         cleanFreqFrameTemp = [0]*((frameSize/2)+1)
-        cleanData = Signal._cleanData
+        cleanData = self._cleanData
         buffer = [0] * frameSize
 
         for itera in range(len(cleanData) - frameSize):
@@ -121,75 +124,75 @@ class Signal:
                 for itera2 in range(len(cleanFreqFrameTemp)):
                     cleanFreqFrame[itera2] = (cleanFreqFrameTemp[itera2] + cleanFreqFrame[itera2]) / 2
                 bufIter=0
-        Signal._cleanTimeFrame = cleanTimeFrame
-        Signal._cleanFreqFrame = cleanFreqFrame
+        self._cleanTimeFrame = cleanTimeFrame
+        self._cleanFreqFrame = cleanFreqFrame
 
     def _calcFreqSpectrums(self):
 
-        Signal._originalDataFreq = abs(np.fft.rfft(Signal._originalData)) /100
-        for iter2 in range(len(Signal._originalDataFreq)):
-            if Signal._originalDataFreq[iter2] < 0:
-                Signal._originalDataFreq[iter2] = Signal._originalDataFreq[iter2] * -1
-        Signal._cleanDataFreq = abs(np.fft.rfft(Signal._cleanData))
-        Signal._meanFrameFreq = abs(np.fft.rfft(Signal._meanFrame))
-        Signal._cleanTimeFrameFreq = abs(np.fft.rfft(Signal._cleanTimeFrame))
+        self._originalDataFreq = abs(np.fft.rfft(self._originalData)) /100
+        for iter2 in range(len(self._originalDataFreq)):
+            if self._originalDataFreq[iter2] < 0:
+                self._originalDataFreq[iter2] = self._originalDataFreq[iter2] * -1
+        self._cleanDataFreq = abs(np.fft.rfft(self._cleanData))
+        self._meanFrameFreq = abs(np.fft.rfft(self._meanFrame))
+        self._cleanTimeFrameFreq = abs(np.fft.rfft(self._cleanTimeFrame))
 
         #JS pradzia
 
     def _rmsOriginal (self):
         sum = 0
-        originalData = Signal._originalData
+        originalData = self._originalData
         for itera in range (len(originalData)):
             sum += ( math.pow(originalData[itera], 2))
         aver = sum / len(originalData)
 
-        Signal.rmsOrig = str(math.sqrt(aver))
+        self.rmsOrig = str(math.sqrt(aver))
 
     def _rmsCleaned (self):
         sum = 0
 
-        cleanData = Signal._cleanData
+        cleanData = self._cleanData
         for itera in range (len(cleanData)):
             sum += ( math.pow(cleanData[itera], 2))
 
         aver = sum / len(cleanData)
 
-        Signal.rmsClean = str(math.sqrt(aver))
+        self.rmsClean = str(math.sqrt(aver))
 
     def _rmsMeanF (self):
         sum = 0
 
-        meanData = Signal._meanFrame
+        meanData = self._meanFrame
 
         for itera in range (len(meanData)):
             sum += ( math.pow(meanData[itera], 2))
 
         aver = sum / len(meanData)
 
-        Signal.rmsMean = str(math.sqrt(aver))
+        self.rmsMean = str(math.sqrt(aver))
 
     def _rmsCleanedSF (self):
         sum = 0
         number = 0
         kiekN = 0
-        cleanTFData = Signal._cleanTimeFrame
+        cleanTFData = self._cleanTimeFrame
         for itera in range (len(cleanTFData)):
             sum += (cleanTFData[itera]*(math.sin(2*3.14*50*0.02)))
             if itera % 1024 == 0:
                 number += sum ** 2
                 kiekN+=1
                 sum = 0
-        Signal.rmsCleanSignal = str((number / kiekN) ** (1.0/2))
+        self.rmsCleanSignal = str((number / kiekN) ** (1.0/2))
 
 
     def _displayTime(self, data, marking = 's'):
-        Signal._lastFigure+=1
-        plt.figure(Signal._lastFigure)
+        self._lastFigure+=1
+        plt.figure(self._lastFigure)
         subplots = 211
         for iter in range(len(data)):
             if(len(data) > 1):
                 plt.subplot(subplots)
-            plt.plot(range(0, len(data[iter]['values'])), data[iter]['values'], Signal._displayParams)
+            plt.plot(range(0, len(data[iter]['values'])), data[iter]['values'], self._displayParams)
             plt.title(data[iter]['title'])
             subplots+=1
 
@@ -209,18 +212,18 @@ class Signal:
 
 
     def _displayFreq(self, data):
-        Signal._lastFigure+=1
+        self._lastFigure+=1
         xAxisMarkerValues = ['0', '5', '10', '15', '20', '25 kHz']
-        plt.figure(Signal._lastFigure)
+        plt.figure(self._lastFigure)
         subplots = 211
 
         for iter in range(len(data)):
             if(len(data) > 1):
                 plt.subplot(subplots)
             Lenght = float(len(data[iter]['values']))
-            localFreMark = (Lenght / 25000) * Signal._freMark
+            localFreMark = (Lenght / 25000) * self._freMark
             plt.bar(localFreMark, max(data[iter]['values']), width=0.8)
-            plt.plot(range(0, int(Lenght)), data[iter]['values'], Signal._displayParams)
+            plt.plot(range(0, int(Lenght)), data[iter]['values'], self._displayParams)
             plt.title(data[iter]['title'])
             xAxisMarkerPlacement = [Lenght * 0, Lenght * 0.2, Lenght * 0.4, Lenght * 0.6, Lenght * 0.8, Lenght * 1]
             plt.xticks(xAxisMarkerPlacement, xAxisMarkerValues, ha='center')
@@ -229,46 +232,46 @@ class Signal:
 
 
     def displayAllData(self):
-        originalDisplay = {'values' : Signal._originalData, 'title' : 'Original signal (Time)'+' RMS='+Signal.rmsOrig+' m/s^2'}
-        cleanDisplay = {'values' : Signal._cleanData, 'title' : 'Cleaned signal (Time)'+' RMS='+Signal.rmsClean+' m/s^2'}
-        Signal._displayTime(self, {0:originalDisplay, 1: cleanDisplay}, 's')
+        originalDisplay = {'values' : self._originalData, 'title' : 'Original signal (Time)'+' RMS='+self.rmsOrig+' m/s^2'}
+        cleanDisplay = {'values' : self._cleanData, 'title' : 'Cleaned signal (Time)'+' RMS='+self.rmsClean+' m/s^2'}
+        self._displayTime({0:originalDisplay, 1: cleanDisplay}, 's')
 
-        meanDisplay = {'values' : Signal._meanFrame, 'title' : 'Mean frame (Time)'+' RMS='+Signal.rmsMean+' m/s^2'}
-        cleanFrameDisplay = {'values' : Signal._cleanTimeFrame, 'title' : 'Cleaned signal frame (Time)'+' RMS='+Signal.rmsCleanSignal+' m/s^2'}
-        Signal._displayTime(self, {0:meanDisplay, 1: cleanFrameDisplay}, 'ms')
+        meanDisplay = {'values' : self._meanFrame, 'title' : 'Mean frame (Time)'+' RMS='+self.rmsMean+' m/s^2'}
+        cleanFrameDisplay = {'values' : self._cleanTimeFrame, 'title' : 'Cleaned signal frame (Time)'+' RMS='+self.rmsCleanSignal+' m/s^2'}
+        self._displayTime({0:meanDisplay, 1: cleanFrameDisplay}, 'ms')
 
-        origFreqDisplay = {'values' : Signal._originalDataFreq, 'title' : 'Original Signal (Freq)'}
-        cleanFreqDisplay = {'values' : Signal._cleanDataFreq, 'title' : 'Cleaned Signal (Freq)'}
-        Signal._displayFreq(self, {0:origFreqDisplay, 1: cleanFreqDisplay})
+        origFreqDisplay = {'values' : self._originalDataFreq, 'title' : 'Original Signal (Freq)'}
+        cleanFreqDisplay = {'values' : self._cleanDataFreq, 'title' : 'Cleaned Signal (Freq)'}
+        self._displayFreq({0:origFreqDisplay, 1: cleanFreqDisplay})
 
-        meanFreqFrameDisplay = {'values' : Signal._meanFrameFreq, 'title' : 'Mean Frame (Freq)'}
-        cleanFreqFrameDisplay = {'values' : Signal._cleanTimeFrameFreq, 'title' : 'Cleaned signal Frame (Freq)'}
-        Signal._displayFreq(self, {0:meanFreqFrameDisplay, 1: cleanFreqFrameDisplay})
+        meanFreqFrameDisplay = {'values' : self._meanFrameFreq, 'title' : 'Mean Frame (Freq)'}
+        cleanFreqFrameDisplay = {'values' : self._cleanTimeFrameFreq, 'title' : 'Cleaned signal Frame (Freq)'}
+        self._displayFreq({0:meanFreqFrameDisplay, 1: cleanFreqFrameDisplay})
 
-        cleanFreqFrame2Display = {'values' : Signal._cleanFreqFrame, 'title' : 'Cleaned Signal Stacked Frequency Frames'}
-        Signal._displayFreq(self, {0: cleanFreqFrame2Display})
+        cleanFreqFrame2Display = {'values' : self._cleanFreqFrame, 'title' : 'Cleaned Signal Stacked Frequency Frames'}
+        self._displayFreq({0: cleanFreqFrame2Display})
 
 
 
 
     def processSignalFromFile(self, location):
-        Signal._loadOriginal_File(self, location)
-        Signal._calcMeanFrame(self)
-        Signal._cleanSignal(self)
-        Signal._stackCleanSignalFrames_Time_Freq(self)
-        Signal._calcFreqSpectrums(self)
+        self._loadOriginal_File(location)
+        self._calcMeanFrame()
+        self._cleanSignal()
+        self._stackCleanSignalFrames_Time_Freq()
+        self._calcFreqSpectrums()
 
-        Signal._rmsOriginal(self)
-        Signal._rmsCleaned(self)
-        Signal._rmsMeanF(self)
-        Signal._rmsCleanedSF(self)
-        Signal.displayAllData(self)
+        self._rmsOriginal()
+        self._rmsCleaned()
+        self._rmsMeanF()
+        self._rmsCleanedSF()
+        self.displayAllData()
 
 def execCalc(event):
 
     signal1 = Signal(inputTime.GetValue(), inputFrame.GetValue(), inputSkip.GetValue(), inputSkipFramesTime.GetValue(), inputFreMark.GetValue(), inputSingleRollTime.GetValue())
     signal1.processSignalFromFile('matavimai/'+ inputFile.GetValue())
-
+    del signal1
     # Draw the plot to the screen
     plt.show()
 
